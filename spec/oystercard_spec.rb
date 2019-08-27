2d88db98e23c228726c29f7c6959ba0f4a297683
Julian George <jules104@gmail.com>
2019-08-27 14:49:41 +0100

    Add oystercard balance test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,17 @@ require 'oystercard'
 
 describe Oystercard do
 
+  it "new oystercard has default balance of 0" do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq (0)
+  end
 end
+
+
+
+=begin
+create Oystercard
+create default balance
+irb create card then check balance
+
+=end
