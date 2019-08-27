aa7ccff33c3731914096719f110086b1e0a47352
James Holton <james_holton@yahoo.co.uk>
2019-08-27 15:12:30 +0100

    written top_up test to test top up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,6 +5,10 @@ describe Oystercard do
   it "new oystercard has default balance of 0" do
     expect(subject.balance).to eq (0)
   end
+
+  it "expect new oystercard to have £10 on, when topped up with 10" do
+    expect(subject.top_up(10)).to eq (10)
+  end
 end
 
 
