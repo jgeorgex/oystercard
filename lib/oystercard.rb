c3d3a7ebe3350b4751393d04fb20c39064b582fa
James Holton <james_holton@yahoo.co.uk>
2019-08-27 16:33:53 +0100

    added topup limit, and breaking deduct test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,18 @@
 class Oystercard
   attr_reader :balance
 
+  BALANCE_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(value)
+    check_limit(value)
     @balance = value + @balance
   end
 
+  def check_limit(value)
+    raise "Balance limit is £#{BALANCE_LIMIT}" if value + @balance > BALANCE_LIMIT
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,10 +15,19 @@ describe Oystercard do
     expect(subject.top_up(10)).to eq (30)
   end
 
-  it "expect an error if balance is more tahn £90" do
-    expect(subject.top_up(91)).to raise_error "Balance limit is £90"
+  it "expect an error if balance is more than £90" do
+    expect{subject.top_up(91)}.to raise_error "Balance limit is £90"
   end
 
+  it "expect oystercard with £20 to error if £80 added" do
+    subject.top_up(20)
+    expect{subject.top_up(80)}.to raise_error "Balance limit is £90"
+  end
+
+  it "card with £50 should reduce to £40 when £10 is spent" do
+    subject.top_up(50)
+    expect(subject.deduct(10)).to eq(40)
+  end
 end
 
 
