b30d798230b7a582b246ca0ba687b8c95bee48a8
James Holton <james_holton@yahoo.co.uk>
2019-08-27 15:06:58 +0100

    created instance variable for balance, initialized to 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,7 @@ require 'oystercard'
 describe Oystercard do
 
   it "new oystercard has default balance of 0" do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq (0)
+    expect(subject.balance).to eq (0)
   end
 end
 
